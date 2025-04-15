25e151c11928ecf331a1e19b5a3e1bcf2de1cb6a
Ian den Hartog <iandenhartog@hotmail.com>
2025-04-15 22:52:21 +0200

    feat: rename url env to upstream
    url is still supported for now. But will be removed later.

--- main.go
+++ main.go
@@ -23,7 +23,15 @@ func run(ctx context.Context) {
 	port := viper.GetInt("port")
 	proxyMetrics := viper.GetBool("proxy_metrics")
 
-	o := overleash.NewOverleash(viper.GetString("url"), tokens, reload)
+	upstream := viper.GetString("upstream")
+	if upstream == "" {
+		upstream = viper.GetString("url")
+		if upstream != "" {
+			log.Warn("The 'url' flag is deprecated. Please use the 'upstream' flag instead.")
+		}
+	}
+
+	o := overleash.NewOverleash(upstream, tokens, reload)
 	o.Start(ctx)
 
 	server.New(o, port, proxyMetrics, ctx).Start()
@@ -47,12 +55,14 @@ func initConfig() {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
-	pflag.String("url", "", "Upstream to load")
-	pflag.String("token", "", "Token to load")
-	pflag.String("port", "5433", "Port")
-	pflag.Int("reload", 0, "Reload")
-	pflag.Bool("verbose", false, "Verbose mode")
-	pflag.Bool("proxy_metrics", false, "Proxy metrics")
+	pflag.String("url", "", "DEPRECATED: Unleash URL (e.g. https://unleash.my-site.com) without /api. Use --upstream instead.")
+	pflag.String("upstream", "", "Unleash upstream URL to load feature flags (e.g. https://unleash.my-site.com) without /api, can be an Unleash instance or Unleash Edge.")
+	pflag.String("token", "", "Comma-separated Unleash client token(s) to fetch feature flag configurations.")
+	pflag.String("port", "5433", "Port number on which Overleash will listen (default: 5433).")
+	pflag.Int("reload", 0, "Reload frequency in minutes for refreshing feature flag configuration (0 disables automatic reloading).")
+	pflag.Bool("verbose", false, "Enable verbose logging to troubleshoot and diagnose issues.")
+	pflag.Bool("proxy_metrics", false, "Proxy metrics requests to the upstream Unleash server (ensure that the correct token is provided in the authorization header).")
+
 	pflag.Parse()
 
 	viper.BindPFlags(pflag.CommandLine)
